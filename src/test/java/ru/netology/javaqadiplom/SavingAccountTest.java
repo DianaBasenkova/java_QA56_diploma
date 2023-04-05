94a71b093be6060d72512a60a625a56d945722df
Netology QA <obgynkostina@gmail.com>
2023-04-05 20:31:52 +0300

    first work with tests

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -18,4 +18,72 @@ public class SavingAccountTest {
 
         Assertions.assertEquals(2_000 + 3_000, account.getBalance());
     }
+
+    @Test
+    public void shouldNotAddMoreThanMaxBalance() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.add(3_000);
+
+        Assertions.assertEquals(2_000 + 3_000, account.getBalance());
+    }
+    @Test
+    public void shouldPayLessThanBalance() {
+        SavingAccount account = new SavingAccount(
+                3_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.pay(1_000);
+
+        Assertions.assertEquals(3_000 - 1_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldPayAllBalanceSum() {
+        SavingAccount account = new SavingAccount(
+                3_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.pay(3_000);
+
+        Assertions.assertEquals(3_000 - 3_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldNotPayMoreBalanceSum() {
+        SavingAccount account = new SavingAccount(
+                3_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.pay(3_001);
+
+        Assertions.assertEquals(3_000, account.getBalance());
+    }
+    @Test
+    public void shouldCountYearChange() {
+        SavingAccount account = new SavingAccount(
+                3_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        account.yearChange();
+
+        Assertions.assertEquals(3_000/100*5, account.yearChange());
+    }
 }
